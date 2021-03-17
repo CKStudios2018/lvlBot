c30912601f6f8625851f611fce2ef9bb5de80cb3
CKStudios2018 <75259904+CKStudios2018@users.noreply.github.com>
2021-03-17 12:11:24 +0100

    Update Index,js CommandHandler

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const jsonfile = require('jsonfile');
 
 const bot = new Discord.Client();
 
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 var stats = {};
 if (fs.existsSync('stats.json')) {
     stats = jsonfile.readFileSync('stats.json');
@@ -49,8 +58,14 @@ bot.on('message', (message) => {
 
     const parts = message.content.split(' ');
 
-    if(parts[0] === '!rank') {
-        message.reply(' you are at level ' + userStats.level)
+    client.on('message', message =>{
+        if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+    
+        if(command === 'ping'){
+            message.reply(' you are at level ' + userStats.level);
     }
 });
 
