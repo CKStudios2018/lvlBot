ef2727a1355feef95911974db509ab829a44fda6
AlkazarG <62423288+AlkazarG@users.noreply.github.com>
2021-05-10 10:48:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -87,24 +87,7 @@ bot.on('message', (message) => {
     if(command === 'rank'){
         message.reply(' you are at level ' + userStats.level);
     } else if(command == 'help'){
-        message.react('✅');
-        const helpEmbed = new Discord.MessageEmbed()
-            .setColor('#AE7BDA')
-            .setTitle('Help')
-            .setDescription('Get some commands help!')
-            .setURL('https://ckstudios2018.github.io/lvlBot/help/')
-            .addFields(
-                {name: 'Help', value: 'This embed', inline: true},
-                {name: 'Rank', value: 'Shows your DLB Rank', inline: true},
-                {name: 'bal', value: 'Shows your balance, rank and total XP', inline: true},
-                {name: 'Shop', value: 'Opens the shop', inline: true},
-                {name: 'buy', value: 'for buying an item', inline: true}
-            )
-            .addField('\u200b', '\u200b')
-            .addField("Website", '[Link](https://ckstudios2018.github.io/lvlBot/)', true)
-            .setTimestamp()
-            .setFooter(`requested by ` + message.author.username + " (" + message.author.id + ")")
-        message.channel.send(helpEmbed);
+        bot.commands.get('help').run(message, args, Discord);
     } else if(command == 'bal'){
         const balanceEmbed = new Discord.MessageEmbed()
             .setColor('#AE7BDD')
