f0b5e5208770320d2ee01577b0c582e20ff4d5c2
AlkazarG <62423288+AlkazarG@users.noreply.github.com>
2021-05-10 10:55:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -93,35 +93,7 @@ bot.on('message', (message) => {
     } else if(command == 'shop'){
         bot.commands.get('shop').execute(message, args, Discord);
     } else if(command == 'buy'){
-        const item = args[0]
-        if(args[0] === 'levelswap'){
-            const swapPrice = 550
-        if (userStats.coins < swapPrice) {
-            message.lineReply('`err:` You dont have enough coins for that!');
-            } else {
-            userStats.coins -= swapPrice;
-            userStats.coins_spent += swapPrice;
-            message.reply('GG, you just bought ' + item);
-            }
-        } else if(args[0] == 'Cookie'){
-            const cookPrice = 6
-        if (userStats.coins < cookPrice) {
-            message.lineReply('`err:` You dont have enough coins for that!');
-            } else {
-            userStats.coins -= cookPrice;
-            userStats.coins_spent += cookPrice;
-            message.reply('GG, you just bought ' + item);
-            }
-        } else if(args[0] == 'Socks'){
-            const sockPrice = 12
-        if (userStats.coins < sockPrice) {
-            message.lineReply('`err:` You dont have enough coins for that!');
-            } else {
-            userStats.coins -= sockPrice;
-            userStats.coins_spent += sockPrice;
-            message.reply('GG, you just bought ' + item);
-            }
-        }
+        bot.commands.get('buy').run(message, args, Discord);
     }
 });
 
